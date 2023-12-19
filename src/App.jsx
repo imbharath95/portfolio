5b02e9b92a433d409ad20db7d88f449301ede4d1
imbharath95 <147942751+imbharath95@users.noreply.github.com>
2023-12-18 22:52:31 -0500

    contact-us page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import "./app.scss";
 import Navbar from "./components/navbar/Navbar";
 import Head from "./components/head/Head";
-import Info from "./components/info/Info"
+import Info from "./components/info/Info";
+import Contact from "./components/contact/Contact";
 const App = () => {
   return <div>
     <section id = "Home Page">
       <Navbar />
       <Head/>
       </section>
-    <section id = "Services"><Info/></section>
-   
-    <section id = "About Us">About Us</section>
-    <section id = "Contact Us">Contact Us</section>
-    
-    
-    {/* <Test/> */}
+    <section id = "Services" ><Info /></section>
+    <section id = "Contact Us"><Contact/></section>
   </div>;
 };
 
